ad5f486620b0c25c4153b2bea4ab4bba8768ee9b
SteveERM <steve.johnston@erm.com>
2024-07-03 11:41:49 +1000

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const config = {
     database: process.env.DB_NAME,
     options: {
         encrypt: true, // Use this if you're on Windows Azure
-        trustServerCertificate: true // Change to true for local dev / self-signed certs
+        trustServerCertificate: true, // Change to true for local dev / self-signed certs
+        connectTimeout: 30000, // 30 seconds
+        requestTimeout: 30000 // 30 seconds
     }
 };
 
